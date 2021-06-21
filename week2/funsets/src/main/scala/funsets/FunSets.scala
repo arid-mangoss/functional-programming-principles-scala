e99ca1db2e719223a1e328dbdd315f65bbce7cf6
arid-mangoss <sssnarr1@gmail.com>
2021-06-20 23:24:48 -0400

    consistent variable naming

--- week2/funsets/src/main/scala/funsets/FunSets.scala
+++ week2/funsets/src/main/scala/funsets/FunSets.scala
@@ -18,31 +18,31 @@ trait FunSets extends FunSetsInterface:
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): FunSet = e => e == elem
+  def singletonSet(elem: Int): FunSet = (e: Int) => e == elem
 
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: FunSet, t: FunSet): FunSet = e => contains(s, e) || contains(t, e)
+  def union(s: FunSet, t: FunSet): FunSet = (e: Int) => contains(s, e) || contains(t, e)
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: FunSet, t: FunSet): FunSet = e => contains(s, e) && contains(t, e)
+  def intersect(s: FunSet, t: FunSet): FunSet = (e: Int) => contains(s, e) && contains(t, e)
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: FunSet, t: FunSet): FunSet = e => contains(s, e) && !contains(t, e)
+  def diff(s: FunSet, t: FunSet): FunSet = (e: Int) => contains(s, e) && !contains(t, e)
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: FunSet, p: Int => Boolean): FunSet = e => contains(s, e) && p(e)
+  def filter(s: FunSet, p: Int => Boolean): FunSet = (e: Int) => contains(s, e) && p(e)
 
 
   /**
@@ -67,12 +67,12 @@ trait FunSets extends FunSetsInterface:
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: FunSet, p: Int => Boolean): Boolean = !forall(s, x => !p(x))
+  def exists(s: FunSet, p: Int => Boolean): Boolean = !forall(s, (x: Int) => !p(x))
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: FunSet, f: Int => Int): FunSet = e => exists(s, a => f(a) == e)
+  def map(s: FunSet, f: Int => Int): FunSet = (e: Int) => exists(s, (x: Int) => f(x) == e)
 
   /**
    * Displays the contents of a set
