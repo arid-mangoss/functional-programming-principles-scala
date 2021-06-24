a990d029524e07dd9e06fbeccddf2c030c76a40f
arid-mangoss <sssnarr1@gmail.com>
2021-06-24 06:17:02 -0400

    non exponential time union

--- week3/objsets/src/main/scala/objsets/TweetSet.scala
+++ week3/objsets/src/main/scala/objsets/TweetSet.scala
@@ -146,7 +146,7 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet:
   def isEmpty: Boolean = false
 
    def union(that: TweetSet): TweetSet = 
-    left.union(right).union(that).incl(elem)
+    left.union(right.union(that)).incl(elem)
 
   // def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = 
   //   if p(elem) then 
